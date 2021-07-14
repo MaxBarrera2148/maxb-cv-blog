847772068c982dbfaa480a242b52ddfe0b55b24c
MaxBarrera2148 <47284028+MaxBarrera2148@users.noreply.github.com>
2021-07-14 11:29:27 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -181,7 +181,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#97266d',
         display: 'standalone',
-        icon: 'static/image_1.png'
+        icon: 'static/favicon.ico'
       }
     },
     'gatsby-plugin-catch-links',
